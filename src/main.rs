28a177672e58796f0abb290d803bfe0af8e630aa
davidTabuyo <dtabum00@estudiantes.unileon.es>
2024-03-27 18:06:26 +0100

    avanzando en funciones

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, io};
+use std::{env, fs::File, io::{self, BufRead, BufReader}};
 
 
 struct Task{
@@ -17,27 +17,55 @@ impl Task {
 }
 // let my_task = Task = Tak::new(ss,ss,ss)
 
-fn get_tasks() -> Vec<Task>{
-    
-    return Task;
+fn get_tasks() -> io::Result<Vec<Task>>{
+    let mut data: Vec<Task> = vec![];
+    let file = File::open("src/to_do_data.txt")?;
+    let reader = BufReader::new(file);
+    for line in reader.lines(){
+        let task_name = line?;
+        let task = Task::new(task_name, false, String::new()); // Puedes ajustar los valores según sea necesario
+        data.push(task);
+    }
+    Ok(data)
 }
 
-fn check_tasks(){
+fn check_tasks(data: &mut Vec<Task>){
+    for index in 0..data.len() {
+        println!("({}) {}",index,data[index].task_name);
+    }
+}
+
+
+fn add_tasks(data: &mut Vec<Task>){
+    println!("EScribe la tarea nueva");
+}
+
+
+fn mark_as_done_task(index:usize){
+    //mark as done task, remove from txt and add to other txt
+}
+
+fn mark_as_done(data: &mut Vec<Task>){
+    println!("Elige la tarea que has acabado");
+    check_tasks(data);
+    let mut imput:String = String::new();
+    io::stdin().read_line(&mut imput).expect("Error al leer la entrada");
+    let imput:usize= imput.trim().parse().expect("Entrada no válida");
+    if imput < data.len(){
+        mark_as_done_task(imput);
+        data.remove(imput);
+    }else{
+        print!("Entrada no válida");
+    }
 
 }
-fn add_tasks(){
-
-}
-fn mark_as_done(){
-
-}
-fn save_changes(){
+fn save_changes(data: &mut Vec<Task>){
     
 }
 
 fn main() {
     //get data from txt
-    let data:Vec<Task> = get_tasks();
+    let mut data:Vec<Task> = get_tasks().unwrap();
     //analize arguments
     let args: Vec<String> = env::args().collect();
     if args.len() == 1{
@@ -51,13 +79,16 @@ fn main() {
             let imput= imput.trim();
             match imput {
                 "0" => {
-                    check_tasks()
+                    check_tasks(&mut data);
+                    break;
                 }
                 "1" => {
-                    add_tasks()
+                    add_tasks(&mut data);
+                    break;
                 }
                 "2" => {
-                    mark_as_done()
+                    mark_as_done(&mut data);
+                    break;
                 }
                 _ => {
                     println!("Entrada invalida, por favor, introduce otro número")
@@ -66,16 +97,17 @@ fn main() {
         }
     }else if args.len()==2{
         //arguments correct, we can execute only one part
-        let chosen = args.get(1).map_or("".to_string(),|s| s.to_string()).as_str();
-        match chosen {
+        let chosen = args.get(1).map_or("".to_string(), |s| s.to_string());
+        let chosen_str=chosen.as_str();
+        match chosen_str {
             "0" => {
-                check_tasks()
+                check_tasks(&mut data)
             }
             "1" => {
-                add_tasks()
+                add_tasks(&mut data)
             }
             "2" => {
-                mark_as_done()
+                mark_as_done(&mut data)
             }
             _ => {
                 println!("Entrada invalida, por favor, introduce otro número")
@@ -86,6 +118,4 @@ fn main() {
         //number of arguments are incorrect
         print!("El número de argumentos es incorrecto . . . ")
     }
-    //save changes
-    save_changes();
 }
